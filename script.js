8f0b852f7deeccee66baa26f34b27a962c2a1431
syrac <carys.cooper@univ.ox.ac.uk>
2024-07-11 18:12:56 +0100

    add board reset

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ var board = new Board();
 function makeMove(pos) {
     board.makemove(pos);
 }
+
+function resetGame() {
+    board = new Board();
+    for(let row = 0; row <= 5; row++) {
+        for(let col = 0; col <= 6; col++) {
+            document.getElementById((row*7 + col).toString()).style.background = "antiquewhite";
+        }
+    }
+}
